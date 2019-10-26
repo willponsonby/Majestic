c8eaf7fa5c101f11f9bd288dbf140bef6e536e96
willponsonby <w.r.a.ponsonby@gmail.com>
2019-10-26 12:40:56 -0400

    fourth commit

--- majestic_start.py
+++ majestic_start.py
@@ -7,11 +7,11 @@ import csv
 
 driver = webdriver.Chrome(r'C:\Users\wrapo\chromedriver.exe')
 
-driver.get("https://www.majestic.co.uk/wine?pageNum=15&pageSize=40")
+driver.get("https://www.majestic.co.uk/wine?pageNum=0&pageSize=40")
 time.sleep(5)
-index = 16
+index = 1
 
-csv_file = open('majestic11.csv', 'w', encoding='utf-8', newline='')
+csv_file = open('majestic_df.csv', 'w', encoding='utf-8', newline='')
 writer = csv.writer(csv_file)
 
 while index <=21:
@@ -122,71 +122,79 @@ while index <=21:
 			except:
 				pass
 
-			more_details = driver.find_element_by_xpath('.//p[@class="product-content__more t-link js-more-toggle"]')
-			more_details.click()
+			try:
+				more_details = driver.find_element_by_xpath('.//p[@class="product-content__more t-link js-more-toggle"]')
+				more_details.click()
 
-			content_table = driver.find_elements_by_xpath('.//table[@class="content-table"]//tr')
-			for x in content_table:
-				td_key_tags = driver.find_elements_by_xpath('.//td[1]')
+				content_table = driver.find_elements_by_xpath('.//table[@class="content-table"]//tr')
+				for x in content_table:
+					td_key_tags = driver.find_elements_by_xpath('.//td[1]')
 
-			key_list = []
-			for x_key in td_key_tags:
-				key_list.append(x_key.text)
+				key_list = []
+				for x_key in td_key_tags:
+					key_list.append(x_key.text)
 
-			for y in content_table:
-				td_value_tags = driver.find_elements_by_xpath('.//td[2]')
+				for y in content_table:
+					td_value_tags = driver.find_elements_by_xpath('.//td[2]')
 
-			value_list = []
-			for y_value in td_value_tags:
-				value_list.append(y_value.text)
+				value_list = []
+				for y_value in td_value_tags:
+					value_list.append(y_value.text)
 
-			if 'TYPE' in key_list:
-				type = value_list[key_list.index('TYPE')]
-				print(value_list[key_list.index('TYPE')])
-			else:
-				type = 'N/A'
-				print('N/A')
-			bottle_dict['Type'] = type
+				if 'TYPE' in key_list:
+					type = value_list[key_list.index('TYPE')]
+					print(value_list[key_list.index('TYPE')])
+				else:
+					type = 'N/A'
+					print('N/A')
+				bottle_dict['Type'] = type
 
-			if 'STYLE' in key_list:
-				style = value_list[key_list.index('STYLE')]
-				print(value_list[key_list.index('STYLE')])
-			else:
-				style = 'N/A'
-				print('N/A')
-			bottle_dict['Style'] = style
+				if 'STYLE' in key_list:
+					style = value_list[key_list.index('STYLE')]
+					print(value_list[key_list.index('STYLE')])
+				else:
+					style = 'N/A'
+					print('N/A')
+				bottle_dict['Style'] = style
 
-			if 'CLOSURE' in key_list:
-				closure = value_list[key_list.index('CLOSURE')]
-				print(value_list[key_list.index('CLOSURE')])
-			else:
-				closure = 'N/A'
-				print('N/A')
-			bottle_dict['Closure'] = closure
+				if 'CLOSURE' in key_list:
+					closure = value_list[key_list.index('CLOSURE')]
+					print(value_list[key_list.index('CLOSURE')])
+				else:
+					closure = 'N/A'
+					print('N/A')
+				bottle_dict['Closure'] = closure
 
-			if 'UNITS' in key_list:
-				units = value_list[key_list.index('UNITS')]
-				print(value_list[key_list.index('UNITS')])
-			else:
-				units = 'N/A'
-				print('N/A')
-			bottle_dict['Units'] = units
+				if 'UNITS' in key_list:
+					units = value_list[key_list.index('UNITS')]
+					print(value_list[key_list.index('UNITS')])
+				else:
+					units = 'N/A'
+					print('N/A')
+				bottle_dict['Units'] = units
 
-			if 'GRAPE' in key_list:
-				grape = value_list[key_list.index('GRAPE')]
-				print(value_list[key_list.index('GRAPE')])
-			else:
-				grape = 'N/A'
-				print('N/A')
-			bottle_dict['Grape'] = grape
+				if 'GRAPE' in key_list:
+					grape = value_list[key_list.index('GRAPE')]
+					print(value_list[key_list.index('GRAPE')])
+				else:
+					grape = 'N/A'
+					print('N/A')
+				bottle_dict['Grape'] = grape
 
-			if 'COUNTRY' in key_list:
-				country = value_list[key_list.index('COUNTRY')]
-				print(value_list[key_list.index('COUNTRY')])
-			else:
-				country = 'N/A'
-				print('N/A')
-			bottle_dict['Country'] = country
+				if 'COUNTRY' in key_list:
+					country = value_list[key_list.index('COUNTRY')]
+					print(value_list[key_list.index('COUNTRY')])
+				else:
+					country = 'N/A'
+					print('N/A')
+				bottle_dict['Country'] = country
+			except:
+				bottle_dict['Type'] = 'N/A'
+				bottle_dict['Style'] = 'N/A'
+				bottle_dict['Closure'] = 'N/A'
+				bottle_dict['Units'] = 'N/A'
+				bottle_dict['Grape'] = 'N/A'
+				bottle_dict['Country'] = 'N/A'
 
 			writer.writerow(bottle_dict.values())
 
@@ -216,68 +224,4 @@ while index <=21:
 		driver.close()
 		break
 
-
-		# Iterate through the list and find the details of each review.
-		#elementToClick = driver.find_element_by_xpath('//h3[@class="space-b--none"]/a')
-		#time.sleep(3)
-
-
-
-
-
-		#elementToClick.click()
-		#for bottle in bottles:
-
-			# Click review button to go to the review section
-			#wine_button = driver.find_element_by_tag_name(bottle).send_keys(Keys.COMMAND + 't') 
-			#wine_button.click()
-		
-			# Initialize an empty dictionary for each review
-			#bottle_dict = {}
-			# Use try and except to skip the review elements that are empty. 
-			# Use relative xpath to locate the title.
-			# Once you locate the element, you can use 'element.text' to return its string.
-			# To get the attribute instead of the text of each element, use 'element.get_attribute()'
-			#try:
-			#	name = review.find_element_by_xpath('.//h1[@itemprop="name" class="product-info__name"]').text
-			#except:
-			#	continue
-
-			#print('Name = {}'.format(name))
-
-			#driver.find_element_by_tag_name('//img[@itemprop="image"]/@src').send_keys(Keys.COMMAND + 'w')
-			#driver.close()
-
-			#stars = review.find_element_by_xpath('.//span[@class="positionAbsolute top0 left0 overflowHidden color_000"]').get_attribute('style')
-			#rating = int(re.findall('\d+', stars)[0])/20
-			#print('Rating = {}'.format(rating))
-
-			#username = review.find_element_by_xpath('.//span[@class="padLeft6 NHaasDS55Rg fontSize_12 pad3 noBottomPad padTop2"]').text
-			#print('Username = {}'.format(username))
-
-			#text = review.find_element_by_xpath('.//span[@class="pad6 onlyRightPad"]').text
-			#print('Text = {}'.format(text))
-
-			
-
-			# OPTIONAL: How can we deal with the "read more" button?
-			
-			# Use relative xpath to locate text, username, date_published, rating.
-			# Your code here
-
-			# Uncomment the following lines once you verified the xpath of different fields
-			
-
-			#bottle_dict['text'] = text
-			#bottle_dict['username'] = username
-			# bottle_dict['date_published'] = date_published
-			#bottle_dict['rating'] = rating
-
-		# We need to scroll to the bottom of the page because the button is not in the current view yet.
-		
-
-		# Locate the next button element on the page and then call `button.click()` to click it.
-		#button = driver.find_element_by_xpath('//li[@class="nextClick displayInlineBlock padLeft5 "]')
-		#button.click()
-		#time.sleep(2)
 
